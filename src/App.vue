d2979959035921c5df8c494ac297a4b9cfccda0e
Marc <colem213@gmail.com>
2017-07-09 19:27:07 -0400

    Inital styling and layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <md-theme md-name="header">
+      <md-whiteframe>
+        <md-toolbar>
+          <md-layout>
+            <h1 class="md-title">Pod Central</h1>
+          </md-layout>
+          <md-layout md-align="end">
+            <md-layout md-flex="15">
+              <md-button href="#">Sign In</md-button>
+            </md-layout>
+            <md-layout md-flex="15">
+              <md-button>Sign Up</md-button>
+            </md-layout>
+          </md-layout>
+        </md-toolbar>
+      </md-whiteframe>
+    </md-theme>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.content {
+  padding: 10px 2%;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <md-layout md-column>
+    <h1>{{ msg }}</h1>
+    <feed-list :feeds="[{'id': 1234}, {'id': 1235}]"></feed-list>
+  </md-layout>
+</template>
+
+<script>
+import FeedList from '@/components/FeedList'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to Pod Central',
+    }
+  },
+  components: {
+    FeedList,
+  },
+}
+</script>
+
